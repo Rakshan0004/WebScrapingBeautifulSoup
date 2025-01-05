1dcc6307fdf4bb5d3095b22cbd7cb21b96d8a932
Chintala Rakshan <136550641+Rakshan0004@users.noreply.github.com>
2025-01-05 20:28:30 +0530

    first

--- webscraping/main.py
+++ webscraping/main.py
@@ -4,6 +4,19 @@ with open('C:/Projects/WebScrapingBeautifulSoup/webscraping/index.html', 'r') as
     content = html_file.read()
 
     soup = BeautifulSoup(content, 'lxml')
-    products_html_tags = soup.find_all('h2')
-    for product in products_html_tags:
-        print(product.text.strip())
+    # products_html_tags = soup.find_all('h2')
+    # for product in products_html_tags:
+    #     print(product.text)
+
+    product_cards = soup.find_all('div', class_='section')
+
+    product_list = []
+
+    for product in product_cards:
+        product_name = product.h2.text.strip()
+        product_price = product.button.text.strip() 
+
+        product_list.append({"name": product_name, "price": product_price})
+    print(product_list)
+
+
